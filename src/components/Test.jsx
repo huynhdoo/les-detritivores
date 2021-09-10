cbfe00b2b3678fce6c99ea9afcaee42ea566f4c3
Amine <imveny>
2021-09-10 14:10:41 +0200

    continue design website

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import logo from "../images/logo-new.png";
 import '../style/main.scss';
-import YouTube from 'react-youtube';
 
 function Test() {
      const [Text, setText] = useState("");
@@ -11,35 +10,94 @@ function Test() {
     }).then((response) => {
       setText(response.data.story.content)
     }).catch(e => console.error(e))
-    const options = {
-      height: '500',
-      width: '500',
-    }
   return (
       <>
-        <section id="start">
+        <div id="start">
             <img src={logo} className="logos" />
-        </section>
+        </div>
 
-        <section>
-            <h1 className="whoarewe">{Text.titleHero}</h1>
-        </section>
-
-        <section className="hero">
-            <p className="textHero">Depuis 2015, notre Société Coopérative d’Intérêt Collectif et entreprise d’insertion, LES DETRITIVORES vous accompagne dans la gestion de vos restes alimentaires (biodéchets) en vous proposant un service professionnel de <span className="bold">sensibilisation</span>, de <span className="bold">collecte</span>, de <span className="bold">valorisation</span> et de <span className="bold">compostage</span> des déchets organiques générés sur le territoire néo-aquitain.</p>
+        <section className="">
+            <h1 className="whoarewe pb-6 orange">
+              {Text.titleHero}
+              <div className="line" />
+            </h1>
+            <div className="d-flex justify-content-center px-3">
+                <p className="textHero text-break light">Depuis 2015, notre Société Coopérative d’Intérêt Collectif et entreprise d’insertion, LES DETRITIVORES vous accompagne dans la gestion de vos restes alimentaires (biodéchets) en vous proposant un service professionnel de <span className="bold">sensibilisation</span>, de <span className="bold">collecte</span>, de <span className="bold">valorisation</span> et de <span className="bold">compostage</span> des déchets organiques générés sur le territoire néo-aquitain.</p>
+            </div>
         </section>
 
         <section className="">
-          <div>
-            <Youtube videoId="_X-pd2E-AxA&t" opts={options} />
+        <div class="container">
+            <div class="row">
+              <div class="col">
+                <iframe
+                  width="853"
+                  height="480"
+                  src={`https://www.youtube.com/embed/_X-pd2E-AxA`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                  className="d-flex justify-content-start rounded"
+                />
+              </div>
+              <div>
+                <h1 className="orange">Nos Usagers</h1>
+                <img src="" alt="" />
+                <p className="sf usages-font">LES RESTAURATEURS</p>
+                <p className="sf usages-font">LA RESTAURATION COLLECTIVE</p>
+                <p className="sf usages-font">LES COLLECTIVITES</p>
+                <p className="sf usages-font">LES PARTICULIERS</p>
+              </div>
+            </div>
           </div>
-          <div>
-            <h1>Nos Usagers</h1>
-            <img src="" alt="" />
-            <p>LES RESTAURATEURS</p>
-            <p>LA RESTAURATION COLLECTIVE</p>
-            <p>LES COLLECTIVITES</p>
-            <p>LES PARTICULIERS</p>
+        </section>
+        <section className="our-engagements">
+          <h1 className="whoarewe pb-6 green">{Text.titleDefault}</h1>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h1 className="orange">ENVIRONNEMENTAUX</h1>
+                <p>Nous œuvrons pour une <span className="bold">utilisation raisonnable</span> des ressources naturelles, nous luttons contre le gaspillage alimentaire et contribuons à la diminution des émissions de CO2.</p>
+                <img src="" />
+              </div>
+              <div class="col">
+                <h1 className="orange">SOCIAUX</h1>
+                <p>Nous créons des emplois pérennes pour des personnes éloignées du <br /> marché du travail. <br />
+                Nos salariés bénéficient d’un <span className="bold">accompagnement professionnel</span> individuel et/ou collectif.
+                <br /><br />Chez les DETRITIVORES, chaque <br /> compétence détenue est valorisée !</p>
+              </div>
+              <div class="col">
+                <h1 className="orange">COOPÉRATIFS</h1>
+                <p>En tant que société coopérative, nous attachons à rassembler l’ensemble des acteurs engagés sur le territoire vers la construction d’une économie plus <span className="bold">inclusive</span>, plus <span className="bold">organique</span> et <span className="bold">plus ouverte</span>.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="offer">
+            <div className="bg-green offers">
+              <h1 className="size">NOS OFFRES</h1>
+            </div>
+
+          <div class="pb-6 d-flex justify-content-center">
+            <h1 className="orange">COLLECTE</h1>
+              <p classname="light">
+                En camions ou à vélos, nos équipes passent régulièrement collecter vos bacs de biodéchets
+              </p>
+            <h1 className="orange">SENSIBILISATION</h1>
+              <p classname="light">
+                Vous accompagner dans votre démarche environnementale et vous sensibiliser est notre priorité !
+              </p>
+            <h1 className="orange">COMPOST</h1>
+              <p classname="light">
+                100% local, coopératif et solidaire: le compost que nous vous proposons a décidément tout pour plaire !
+              </p>   
+          </div>
+        </section>
+        <section>
+          <h1 className="orange">ILS NOUS FONT CONFIANCE</h1>
+          <div className="pb-6 d-flex justify-content-center">
+            
           </div>
         </section>
     </>
