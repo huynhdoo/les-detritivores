973c0cb876f41730918d06ba52ea7f6d5623a12e
Amine <imveny>
2021-09-13 10:13:17 +0200

    rename class -> className,

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -21,14 +21,14 @@ function Test() {
               {Text.titleHero}
             </h1>
             <div className="d-flex justify-content-center px-3">
-                <p className="textHero text-break light">Depuis 2015, notre Société Coopérative d’Intérêt Collectif et entreprise d’insertion, LES DETRITIVORES vous accompagne dans la gestion de vos restes alimentaires (biodéchets) en vous proposant un service professionnel de <span className="bold">sensibilisation</span>, de <span className="bold">collecte</span>, de <span className="bold">valorisation</span> et de <span className="bold">compostage</span> des déchets organiques générés sur le territoire néo-aquitain.</p>
+                <p className="textHero text-wrap text-break light">Depuis 2015, notre Société Coopérative d’Intérêt Collectif et entreprise d’insertion, LES DETRITIVORES vous<br/> accompagne dans la gestion de vos restes alimentaires (biodéchets) en vous proposant un service <br />professionnel de <span className="bold">sensibilisation</span>, de <span className="bold">collecte</span>, de <span className="bold">valorisation</span> et de <span className="bold">compostage</span> des déchets organiques<br /> générés sur le territoire néo-aquitain.</p>
             </div>
         </section>
 
         <section className="">
-        <div class="container-xxl d-flex">
-            <div class="row">
-              <div class="col">
+        <div className="container-xxl d-flex">
+            <div className="row">
+              <div className="col">
                 <iframe
                   width="853"
                   height="510"
@@ -40,7 +40,7 @@ function Test() {
                   className="d-flex justify-content-start rounded"
                 />
               </div>
-              <div class="col ml-mb">
+              <div className="col ml-mb">
                 <h1 className="orange">Nos Usagers</h1>
                 <div className="rounded-circle bg-orange circle">
                     <Icons icon="people" />
@@ -55,20 +55,20 @@ function Test() {
         </section>
         <section className="our-engagements">
           <h1 className="whoarewe pb-6 green">{Text.titleDefault}</h1>
-          <div class="container">
-            <div class="row">
-              <div class="col">
+          <div className="container">
+            <div className="row">
+              <div className="col">
                 <h1 className="orange">ENVIRONNEMENTAUX</h1>
                 <p>Nous œuvrons pour une <span className="bold">utilisation raisonnable</span> des ressources naturelles, nous luttons contre le gaspillage alimentaire et contribuons à la diminution des émissions de CO2.</p>
                 <img src="" />
               </div>
-              <div class="col">
+              <div className="col">
                 <h1 className="orange">SOCIAUX</h1>
                 <p>Nous créons des emplois pérennes pour des personnes éloignées du <br /> marché du travail. <br />
                 Nos salariés bénéficient d’un <span className="bold">accompagnement professionnel</span> individuel et/ou collectif.
                 <br /><br />Chez les DETRITIVORES, chaque <br /> compétence détenue est valorisée !</p>
               </div>
-              <div class="col">
+              <div className="col">
                 <h1 className="orange">COOPÉRATIFS</h1>
                 <p>En tant que société coopérative, nous attachons à rassembler l’ensemble des acteurs engagés sur le territoire vers la construction d’une économie plus <span className="bold">inclusive</span>, plus <span className="bold">organique</span> et <span className="bold">plus ouverte</span>.</p>
               </div>
@@ -80,7 +80,7 @@ function Test() {
               <h1 className="size">NOS OFFRES</h1>
             </div>
 
-          <div class="pb-6 d-grid gap-3 justify-content-center mt-6">
+          <div className="pb-6 d-grid gap-3 justify-content-center mt-6">
             <div>
               <div>
                   <p classname="light">
@@ -103,11 +103,11 @@ function Test() {
             </div>
           </div>
         </section>
-        <div class="spacing bg-green" />
-        <section>
+        <div className="spacing bg-green" />
+        <section className="trust">
           <h1 className="orange">ILS NOUS FONT CONFIANCE</h1>
-          <div className="pb-6 d-flex justify-content-center">
-            
+          <div className="pb-6 d-flex justify-content-center p-2">
+              <div className="trusting bg-green"></div>
           </div>
         </section>
 
