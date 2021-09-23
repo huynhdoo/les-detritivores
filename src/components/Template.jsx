44c6626c7ade2fc52dfa211d271303c13580b7f1
amineohn <amineprojet7@gmail.com>
2021-09-23 11:45:21 +0200

    add wheelchair icon (tests) + sound implementation !

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,10 +1,34 @@
 import Icons from './Icons';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useSound from 'use-sound';
+import links from '../sounds/sound.mp3';
+
+export default function useKeypress(key, action) {
+  useEffect(() => {
+    function onKeyup(e) {
+      if (e.key === key) action();
+    }
+    window.addEventListener('keyup', onKeyup);
+    return () => window.removeEventListener('keyup', onKeyup);
+  }, []);
+}
 const Template = ({ children }) => {
+  const [play] = useSound(links);
+
+  useKeypress('s', () => {
+    play();
+  });
   return (
     <>
       <div id="start">
         <Icons icon="logo" className="logos" />
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col col-lg-2">
+          <div className="bg-green wheelchair">
+            <Icons icon="wheelchair" />
+          </div>
+        </div>
       </div>
       {children}
       <footer id="footer">
