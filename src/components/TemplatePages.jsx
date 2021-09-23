399087f539a455ebba93214626a8a278f67fcb0e
amineohn <amineprojet7@gmail.com>
2021-09-23 10:33:55 +0200

    add eventListener with const handResize.

--- src/components/TemplatePages.jsx
+++ src/components/TemplatePages.jsx
@@ -4,13 +4,25 @@ import { Link } from 'gatsby';
 const TemplatePages = ({ children, link, isFixed }) => {
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const handleResize = () => {
+    // You save my day. https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
+    if (window.innerWidth < 769) {
+      setIsMobile(true);
+      setIsDesktop(false);
+    } else {
+      setIsMobile(false);
+      setIsDesktop(true);
+    }
+  };
   useEffect(() => {
     if (window.innerWidth > 769) {
       setIsDesktop(true);
       setIsMobile(false);
+      window.addEventListener('resize', handleResize);
     } else {
       setIsMobile(true);
       setIsDesktop(false);
+      window.addEventListener('resize', handleResize);
     }
   }, []);
   return (
