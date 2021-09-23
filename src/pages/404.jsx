60732c4da72d24c10c238abc4067a352b7a249fe
amineohn <amineprojet7@gmail.com>
2021-09-23 11:09:10 +0200

    404 page design + is footer is fixed or not on mobile or desktop.

---
+++ src/pages/404.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import { Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import { TemplatePages } from '../components/TemplatePages';
+
+const Page404 = () => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Page not found</title>
+        <html lang="fr" />
+        <meta name="description" content="Page not found" />
+      </Helmet>
+      <TemplatePages isFixed="true" link="/">
+        <Container>
+          <div class="row">
+            <div class="col-sm disable"></div>
+            <div class="col-sm">
+              <Fade bottom duration={1000} delay={500} distance="30px">
+                <h1 className="hero-title text-center c-s">
+                  Désolé, cette page n'existe pas{' '}
+                  <span role="img" aria-label="emoji">
+                    😞
+                  </span>
+                </h1>
+              </Fade>
+            </div>
+            <div class="col-sm disable"></div>
+          </div>
+        </Container>
+      </TemplatePages>
+    </>
+  );
+};
+export default Page404;
