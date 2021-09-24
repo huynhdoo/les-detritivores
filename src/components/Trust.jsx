ffe525f6fdefd809927d615c5c46289eb41c4eb2
amineohn <amineprojet7@gmail.com>
2021-09-24 08:31:36 +0200

    add slider

--- src/components/Trust.jsx
+++ src/components/Trust.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Icons from './Icons';
 const Trust = () => {
+  const images = ['logo', 'logo', 'logo', 'logo', 'logo', 'logo'];
+  const delay = 2500;
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () => setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1)),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
   return (
     <>
       <h1 className="orange size">ILS NOUS FONT CONFIANCE</h1>
       <div className="pb-6 d-flex justify-content-center p-2">
-        <div className="trusting bg-green"></div>
+        <div className="trusting bg-green">
+          <div
+            className="partener slideshowSlider"
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+          >
+            {images.map((Image, index) => (
+              <Icons className="sliding" key={index} icon={Image} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
