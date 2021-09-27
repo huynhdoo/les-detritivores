97dd7643429d2aafbba34bd8cdabda12a7a3429f
amineohn <amineprojet7@gmail.com>
2021-09-27 14:12:30 +0200

    fix footer, and remove handleResize.

--- src/components/TemplatePages.jsx
+++ src/components/TemplatePages.jsx
@@ -2,29 +2,6 @@ import Icons from './Icons';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 const TemplatePages = ({ children, link, isFixed }) => {
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  const handleResize = () => {
-    // You save my day. https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
-    if (window.innerWidth < 769) {
-      setIsMobile(true);
-      setIsDesktop(false);
-    } else {
-      setIsMobile(false);
-      setIsDesktop(true);
-    }
-  };
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-      window.addEventListener('resize', handleResize);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-      window.addEventListener('resize', handleResize);
-    }
-  }, []);
   return (
     <>
       <div id="start">
@@ -37,7 +14,7 @@ const TemplatePages = ({ children, link, isFixed }) => {
         </div>
       </div>
       {children}
-      <footer id="footer" className={isDesktop ? `fixed-bottom` : ``}>
+      <footer id="footer" className={isFixed ? `fixed-bottom` : ``}>
         <div className="d-flex justify-content-end">
           <Icons icon="logo" className="logo-footer" />
         </div>
