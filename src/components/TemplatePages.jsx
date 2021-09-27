1ac9f9754a609702068adf46308bbe181c40efcf
amineohn <amineprojet7@gmail.com>
2021-09-27 14:12:46 +0200

    remove unuse import

--- src/components/TemplatePages.jsx
+++ src/components/TemplatePages.jsx
@@ -1,5 +1,5 @@
 import Icons from './Icons';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 const TemplatePages = ({ children, link, isFixed }) => {
   return (
