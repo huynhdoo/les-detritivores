b2c9a3a3bb777796a08faa7c54f9cca6df6d41e4
amineohn <amineprojet7@gmail.com>
2021-09-27 15:24:48 +0200

    fixed error vercel.

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -4,17 +4,6 @@ import Mobile from './Mobile';
 import { isMobile } from 'react-device-detect';
 
 const Offer = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const { innerWidth } = window;
-      setScreenWidth(innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
   return isMobile ? <Mobile /> : <Desktop />;
 };
 export default Offer;
