efcb80c0d814a2bf8bfce9535b4b67c9874b0618
amineohn <amineprojet7@gmail.com>
2021-10-18 13:49:17 +0200

    change semicolon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,7 +23,7 @@ function Home() {
   const [Text, setText] = useState('');
   axios
     .get(
-      'https://api.storyblok.com/v2/cdn/stories/preview?version=draft&token=4Sl5OG2kesCX0K97UTd0Wwtt&cv=1631095460'
+      `https://api.storyblok.com/v2/cdn/stories/preview?version=draft&token=4Sl5OG2kesCX0K97UTd0Wwtt&cv=1631095460`
     )
     .then((response) => {
       setText(response.data.story.content);
