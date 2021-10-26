b0e16ed7f2ecc209329d8d320b94f0a6309f895c
amineohn <amineprojet7@gmail.com>
2021-10-26 15:49:16 +0200

    add sensibilisation storyblok integration.

--- src/pages/sensibilisation.jsx
+++ src/pages/sensibilisation.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import FadeIn from 'react-fade-in';
 import { TemplatePages } from '../components/TemplatePages';
 import Image from '../components/Image';
 import Page from 'react-page-loading';
+import Parser from 'react-html-parser';
+
+import axios from 'axios';
 const Sensibilisation = () => {
+  const [Text, setText] = useState('');
+  axios
+    .get(
+      `https://api.storyblok.com/v2/cdn/stories/preview?version=draft&token=4Sl5OG2kesCX0K97UTd0Wwtt`
+    )
+    .then((response) => {
+      setText(response.data.story.content);
+    })
+    .catch((e) => console.error(e));
   return (
     <>
       <Helmet>
@@ -23,31 +35,20 @@ const Sensibilisation = () => {
                 </div>
                 <div className="col c-resp deskt-720">
                   <h1 className="orange big-font-size-xxl font-size-smm rotate pb-2 text-start yup">
-                    SENSIBILISATION
+                    {Text.titleSensibilisation}
                     <span className="linesmall-c bg-black" />
                   </h1>
                   <div className="sensibilisation">
                     <p className="bold big-font-size-md choice-text">
-                      Choisissez nos actions de sensibilisation pour mobiliser l’ensemble
-                      <br />
-                      de vos collaborateurs, convives et équipes autour de la notion de <br />
-                      biodéchets et de recyclage !
+                      {Parser(Text.textSensibilisation)}
                     </p>
                   </div>
                   <div className="sens">
-                    <p className="light text-assign">
-                      Réalisée sur le temps du midi <br /> Présentation de l’écosystème de la SCIC
-                      SA LES <br />
-                      DETRITIVORES
-                      <br /> Présentation des étapes d’élaboration de notre compost <br />
-                      Présentation de notre plateforme de compostage 100% <br />
-                      bordelaise
-                    </p>
+                    <p className="light text-assign">{Parser(Text.text2Sensibilisation)}</p>
                   </div>
                   <div className="atelier">
                     <p className="bold big-font-size-md choice-text">
-                      Jeux, animations, ateliers autour du tri et du recyclage : notre <br />{' '}
-                      sensibilisation se veut conviviale et riche en partages !
+                      {Parser(Text.gameSensibilisation)}
                     </p>
                   </div>
                 </div>
