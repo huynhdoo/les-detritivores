738495fda88e81d7c69a1dc0384396753e466b54
amineohn <amineprojet7@gmail.com>
2021-11-04 13:50:28 +0100

    separate token and endpoint.

--- libs/storyblok.ts
+++ libs/storyblok.ts
@@ -1,16 +1,15 @@
 import axios from "axios";
 import StoryBlokClient, { Richtext } from "storyblok-js-client";
 import Parser from "react-html-parser";
+const token = "4Sl5OG2kesCX0K97UTd0Wwtt";
+const endpoint = `https://api.storyblok.com/v2/cdn/stories/preview?version=draft&token=${token}`;
 export const getTexts = async () => {
-  return await axios.get(
-    `https://api.storyblok.com/v2/cdn/stories/preview?version=draft&token=4Sl5OG2kesCX0K97UTd0Wwtt`
-  );
+  return await axios.get(endpoint);
 };
 
 export const useRichText = (data: Richtext) => {
-  const TOKEN = "4Sl5OG2kesCX0K97UTd0Wwtt";
   const story = new StoryBlokClient({
-    accessToken: TOKEN,
+    accessToken: token,
   });
   return Parser(story.richTextResolver.render(data));
 };
