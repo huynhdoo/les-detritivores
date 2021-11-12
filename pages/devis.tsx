1835c20f9a91ba451eb8eea974cf38fbd618be83
amineohn <amineprojet7@gmail.com>
2021-11-12 10:33:42 +0100

    miss match responsiveness grids

--- pages/devis.tsx
+++ pages/devis.tsx
@@ -150,7 +150,7 @@ const Devis: NextPage = () => {
         distance="30px"
       >
         <div className="max-w-screen my-3 justify-center content-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 mx-5 my-2 space-x-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 sm:grid-cols-2 mx-0 sm:mx-5 md:mx-5 my-2 space-x-10">
             <div className="flex justify-center">
               <form
                 className="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 xl:w-100 md:w-96 sm:grid-cols-1 gap-x-2 gap-y-3 justify-center"
@@ -271,7 +271,7 @@ const Devis: NextPage = () => {
                 <div className="flex flex-col justify-center">
                   <label>Message:*</label>
                   <textarea
-                    className="w-full h-auto px-3 py-2 focus:outline-none bg-white dark:bg-gray-900 border-2 border-orangeDDTV w-26 h-12 p-3 rounded-md transition text-black dark:text-white"
+                    className="w-full h-auto px-3 py-2 focus:outline-none bg-white dark:bg-gray-900 border-2 border-orangeDDTV w-26 p-3 rounded-md transition text-black dark:text-white"
                     {...register("message", { required: true })}
                     autoComplete="off"
                     onChange={onMessageChange}
@@ -296,7 +296,7 @@ const Devis: NextPage = () => {
               {data ? (
                 <>
                   <div className="space-y-4">
-                    <h1 className="text-left pb-2 md:text-6xl text-2xl font-bold text-orangeDDTV -rotate-2">
+                    <h1 className="text-left pb-2 text-2xl lg:text-3xl md:!text-4xl sm:text-3xl xl:!text-6xl 2xl:!text-6xl font-bold text-orangeDDTV -rotate-2">
                       {data?.content.MoneyTitle}
                       <div className="bg-growing-underline-black hidden">
                         &nbsp;
